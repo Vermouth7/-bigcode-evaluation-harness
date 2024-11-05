9dc4081a10e23ba10aa648a9e15d442d17f2ad82
Vermouth7 <thisisabadtry@outlook.com>
2024-11-05 20:33:23 +0800

    init

---
+++ run.sh
@@ -0,0 +1,31 @@
+# CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --main_process_port 29502 --num_processes=4  main.py \
+#   --model /data1/chh/models/meta-llama/Meta-Llama-3-8B-Instruct \
+#   --tasks ds1000-all-completion \
+#   --n_samples 1 \
+#   --max_length_generation 512 \
+#   --temperature 0.1 \
+#   --do_sample True \
+#   --batch_size 1 \
+#   --precision bf16 \
+#   --allow_code_execution \
+#   --generation_only \
+#   --save_generations \
+#   --save_generations_path /home/chh/repos/my_ctg/results/ds1000/ \
+#   --my_mode 0 \
+#   --split_file /home/chh/repos/my_ctg/instructions/ds1000/ds1000_2steps_llama.json 
+
+CUDA_VISIBLE_DEVICES=0 accelerate launch --main_process_port 29502 --num_processes=1  main.py \
+  --model /data1/chh/models/meta-llama/Meta-Llama-3-8B-Instruct \
+  --tasks humaneval \
+  --n_samples 10 \
+  --max_new_tokens 512 \
+  --temperature 0.1 \
+  --do_sample True \
+  --batch_size 1 \
+  --precision bf16 \
+  --allow_code_execution \
+  --save_generations \
+  --save_generations_path /home/chh/repos/my_ctg/results/humaneval/ \
+  --my_mode 0 \
+  --split_file /home/chh/repos/my_ctg/instructions/humaneval/humaneval_2steps_llama_2.json
+
